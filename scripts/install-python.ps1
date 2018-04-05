f216d134a02d0363816c55c94a19ffbe97aabb51
Vijayboopathy <vijayboopathy.e@gmail.com>
2018-04-05 11:40:48 +0530

    ansible remote

--- scripts/install-python.ps1
+++ scripts/install-python.ps1
@@ -6,4 +6,3 @@ choco install -y python2
 [Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Python27;C:\Python27\Scripts")
 python -m pip install --upgrade pip
 pip install ansible
-ansible-playbook C:\Windows\Temp\setup.yml 
