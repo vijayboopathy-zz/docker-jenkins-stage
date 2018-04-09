318c64a48a7b1261ce8a4b3a9929009944c2c7bc
Vijayboopathy <vijayboopathy.e@gmail.com>
2018-04-09 19:17:55 +0530

    fix

--- scripts/install-python.ps1
+++ scripts/install-python.ps1
@@ -6,5 +6,3 @@ $script.DownloadString("https://chocolatey.org/install.ps1")
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco upgrade chocolatey
 choco install -y python2
-Invoke-WebRequest -Uri $sqlurl -OutFile $sqldest
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
