604386755f639b06209f9f71a50fc60edf99ef82
Vijayboopathy <vijayboopathy.e@gmail.com>
2018-04-09 18:52:08 +0530

    added installation of sql server

--- scripts/install-python.ps1
+++ scripts/install-python.ps1
@@ -2,10 +2,6 @@
 
 $script  = New-Object Net.WebClient
 $script.DownloadString("https://chocolatey.org/install.ps1")
-$commondest = "C:\Windows\Temp"
-$sqlurl  = "https://download.microsoft.com/download/6/D/9/6D90C751-6FA3-4A78-A78E-D11E1C254700/SQLServer2014SP2-KB3171021-x64-ENU.exe"
-$sqldest = "$commondest\SQLServer2014SP2-KB3171021-x64-ENU.exe"
-$start_time = Get-Date 
 
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco upgrade chocolatey
--- scripts/install-sql.ps1
+++ scripts/install-sql.ps1
@@ -1,8 +1,9 @@
 $commondest = "C:\Windows\Temp"
 $sqlurl  = "https://download.microsoft.com/download/6/D/9/6D90C751-6FA3-4A78-A78E-D11E1C254700/SQLServer2014SP2-KB3171021-x64-ENU.exe"
-$sqldest = "$commondest\SQLServer2014SP2-KB3171021-x64-ENU.exe"
+$sqldest = "$commondest\SQLServer2014SP2.exe"
 $start_time = Get-Date
 
 Invoke-WebRequest -Uri $sqlurl -OutFile $sqldest
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-
+Set-Location -Path $commondest
+SQLServer2014SP2.exe /ConfigurationFile=ConfigurationFile.ini
